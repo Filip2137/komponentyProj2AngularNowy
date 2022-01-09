2da8fa811c05c7e559240dc71ae556227a2a9ca2
michal.boltowicz <micbo26@gmail.com>
2022-01-10 00:43:48 +0100

    service is done
    change interfaces to classes

---
+++ src/models/Seance.ts
@@ -0,0 +1,9 @@
+interface Seance {
+  id: number;
+  date: string;
+  hour: string;
+  roomID: number;
+  movieID: number;
+  amountOf_sold_tickets: number;
+  amountOf_available_tickets: number;
+}
--- src/services/database-service.service.ts
+++ src/services/database-service.service.ts
@@ -15,11 +15,79 @@ constructor(private http: HttpClient) { }
   public getMovies() : Observable<Room[]>{
     return this.http.get<Room[]>(
       `${this.URL}/Movie`
-    );
+    )
   }
-  public postMovie() : Observable<Room[]>{
+  public postMovie(movie: Movie){
+    return this.http.post(`${this.URL}/Movie`,
+    movie,
+    )
+  }
+  public putMovie(movie: Movie, id: number){
+    return this.http.post(`${this.URL}/Movie/${id}`,
+    movie,
+    )
+  }
+  public deleteMovie(id: number){
+    return this.http.delete(`${this.URL}/Movie/${id}`,
+    )
+  }
+
+  public getRooms() : Observable<Room[]>{
     return this.http.get<Room[]>(
-      `${this.URL}/Movie`
-    );
+      `${this.URL}/Room`
+    )
+  }
+  public postRoom(room: Room){
+    return this.http.post(`${this.URL}/Room`,
+    room,
+    )
+  }
+  public putRoom(room: Room, id: number){
+    return this.http.post(`${this.URL}/Room/${id}`,
+    room,
+    )
+  }
+  public deleteRoom(id: number){
+    return this.http.delete(`${this.URL}/Room/${id}`,
+    )
+  }
+
+  public getSeances() : Observable<Seance[]>{
+    return this.http.get<Seance[]>(
+      `${this.URL}/Seance`
+    )
+  }
+  public postSeance(seance: Seance){
+    return this.http.post(`${this.URL}/Seance`,
+    seance,
+    )
+  }
+  public putSeance(seance: Seance, id: number){
+    return this.http.post(`${this.URL}/Seance/${id}`,
+    seance,
+    )
+  }
+  public deleteSeance(id: number){
+    return this.http.delete(`${this.URL}/Seance/${id}`,
+    )
+  }
+  public getTickets() : Observable<Ticket[]>{
+    return this.http.get<Ticket[]>(
+      `${this.URL}/Ticket`
+    )
+  }
+  public postTicket(ticket: Ticket){
+    return this.http.post(`${this.URL}/Ticket`,
+    ticket,
+    )
+  }
+  public putTicket(ticket: Ticket, id: number){
+    return this.http.post(`${this.URL}/Ticket/${id}`,
+    ticket,
+    )
+  }
+  public deleteTicket(id: number){
+    return this.http.delete(`${this.URL}/Ticket/${id}`,
+    )
   }
 }
