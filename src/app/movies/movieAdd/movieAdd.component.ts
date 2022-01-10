810ecf841be6956d4e6b24b57659457fee0d51a9
Filip2137 <filip.andrejuk@gmail.com>
2022-01-10 14:11:47 +0100

    Adding almost work

--- src/app/movies/movieAdd/movieAdd.component.ts
+++ src/app/movies/movieAdd/movieAdd.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {FormBuilder,FormGroup} from '@angular/forms';
+import { DatabaseServiceService } from 'src/services/database-service.service';
+import {Movie} from 'src/models/Movie';
 @Component({
   selector: 'app-movieAdd',
   templateUrl: './movieAdd.component.html',
@@ -7,7 +9,8 @@ import {FormBuilder,FormGroup} from '@angular/forms';
 })
 export class MovieAddComponent implements OnInit {
   formValue!: FormGroup;
-  constructor(private formbuilder: FormBuilder){}
+  movie: Movie=new Movie(0,'a',0,'a','a');
+  constructor(private formbuilder: FormBuilder, private api:DatabaseServiceService){}
 
   ngOnInit(): void {
     this.formValue=this.formbuilder.group({
@@ -15,6 +18,23 @@ export class MovieAddComponent implements OnInit {
       duration: [''],
       image:[''],
       description:['']
+    })
+  }
+  postMovie()
+  {
+    this.movie.title=this.formValue.value.title;
+    this.movie.description=this.formValue.value.description;
+    this.movie.image=this.formValue.value.image;
+    this.movie.duration=this.formValue.value.duration;
+
+    this.api.postMovie(this.movie)
+    .subscribe(res=>{
+      console.log(res);
+      alert("Added Movie Successfully")
+      this.formValue.reset();
+    },
+    err=>{
+      alert("Mistake Oh no")
     })
   }
 
