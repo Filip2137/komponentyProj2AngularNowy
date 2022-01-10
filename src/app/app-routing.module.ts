07981576f4b31fbb5839cfa896aa859a79cd493d
Gabriel <camed.tf@gmail.com>
2022-01-10 13:14:44 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Filip2137/komponentyProj2AngularNowy""
    This reverts commit 0b8ed31cf2bd8b3fac478c8466580d1bf6d0fe15.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MovieAddComponent } from './movies/movieAdd/movieAdd.component';
 import { MoviesComponent } from './movies/movies.component';
 import { SeancesComponent } from './seances/seances.component';
+import { SeanceAddComponent } from './seances/seance-add/seance-add.component';
 
 const routes: Routes = [
   {path:'movies',component:MoviesComponent},
   {path: 'movies/addMovie', component: MovieAddComponent},
   {path:'seances',component:SeancesComponent},
+  {path:'seances/addSeance',component: SeanceAddComponent },
   {path: '**', redirectTo: '/'}
 ];
 
